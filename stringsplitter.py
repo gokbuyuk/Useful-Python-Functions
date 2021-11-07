bce56764ad1fadef2877cdfe9c7e401dda4a915b
Gokcen Buyukbas <gokcenbuyukbas@gmail.com>
2021-11-07 13:32:27 -0500

    Create stringsplitter.py
    A split function that splits strings by capital letters

---
+++ stringsplitter.py
@@ -0,0 +1,6 @@
+#Split strings by capital letter
+def string_splitter(word, glue):
+  """take a word with a capital initial and split 
+  it by the capital letters appering in the word 
+  and paste them with glue""" 
+  return glue.join([letter + rest for letter, rest in zip(re.findall(r'[A-Z]', word),re.split(r'[A-Z]', word)[1:])])
