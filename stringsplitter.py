13cc2b1069d0280a434a3f928caf4caf7c12900d
Gokcen Buyukbas <gokcenbuyukbas@gmail.com>
2021-11-07 14:17:28 -0500

    Update stringsplitter.py

--- stringsplitter.py
+++ stringsplitter.py
@@ -4,3 +4,8 @@ def string_splitter(word, glue):
   it by the capital letters appering in the word 
   and paste them with glue""" 
   return glue.join([letter + rest for letter, rest in zip(re.findall(r'[A-Z]', word),re.split(r'[A-Z]', word)[1:])])
+
+if __name__ == __main__:
+  print(string_splitter("MultipleWordString", " "))
+
+
