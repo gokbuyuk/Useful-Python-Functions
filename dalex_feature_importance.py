7b005a05c19a1da0bc37fea5e82ee73787ccf70a
Gokcen Buyukbas <gokcenbuyukbas@gmail.com>
2022-08-17 11:30:43 -0500

    Update dalex_feature_importance.py

--- dalex_feature_importance.py
+++ dalex_feature_importance.py
@@ -3,14 +3,19 @@ import dalex as dx
 features = []
 exp = dx.Explainer(model, X_train[features], y_train)
 vi = exp.model_parts(loss_function='1-auc')
-order_features_model1 = vi.result.sort_values(by='dropout_loss', ascending=False)
+dx_feature_imp = vi.result.sort_values(by='dropout_loss', ascending=False)
+base_score = dx_feature_imp.loc[dx_feature_imp['variable'] == '_full_model_', 	'dropout_loss'].tolist()[0]
+dx_feature_imp['dropout_loss'] = dx_feature_imp['dropout_loss'] - base_score 
 
-feature_imp = order_features_model1[order_features_model1['variable'].isin(features)]
+dx_feature_imp = dx_feature_imp[dx_feature_imp['variable'].isin(features)]
 for feature in features:
     temp_df = Xy_train[[feature, target]].dropna()
     r, p = stats.pearsonr(temp_df[feature], temp_df[target])
-    feature_imp.loc[feature_imp['variable']==feature, 'Direction'] = np.where( r< 0,  
+    dx_feature_imp.loc[dx_feature_imp['variable']==feature, 'Direction'] = np.where( r< 0,  
     'Negative', 'Positive')
     
-sns.barplot(data=feature_imp, y='variable', x='dropout_loss', hue='Direction', dodge=False)
-
+sns.barplot(data=dx_feature_imp, y='variable', x='dropout_loss', hue='Direction', dodge=False)
+plt.title('Dropout loss for features in the model')
+plt.xlabel('1-AUC')
+plt.ylabel('Feature')
+plt.legend(loc='center right')
